f99c4683798206658714c5ef768b37841d7576d3
kartikver15gr8 <vermakartikey786@gmail.com>
2024-06-19 15:39:10 +0530

    bug fix 'updatelinks'

--- app/api/getlinks/route.ts
+++ app/api/getlinks/route.ts
@@ -25,4 +25,6 @@ export async function GET(req: NextRequest) {
       }
     }
   }
+  // return if the links are not present
+  return Response.json({ msg: "No links" });
 }
---
+++ prisma/migrations/20240619100429_removed/migration.sql
@@ -0,0 +1,35 @@
+-- DropIndex
+DROP INDEX "Links_figma_key";
+
+-- DropIndex
+DROP INDEX "Links_github_key";
+
+-- DropIndex
+DROP INDEX "Links_gumroad_key";
+
+-- DropIndex
+DROP INDEX "Links_hashnode_key";
+
+-- DropIndex
+DROP INDEX "Links_instagram_key";
+
+-- DropIndex
+DROP INDEX "Links_linkedin_key";
+
+-- DropIndex
+DROP INDEX "Links_medium_key";
+
+-- DropIndex
+DROP INDEX "Links_producthunt_key";
+
+-- DropIndex
+DROP INDEX "Links_substack_key";
+
+-- DropIndex
+DROP INDEX "Links_twitch_key";
+
+-- DropIndex
+DROP INDEX "Links_twitter_key";
+
+-- DropIndex
+DROP INDEX "Links_youtube_key";
