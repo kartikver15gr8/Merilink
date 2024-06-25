70232232d083fb0d941b551b8c8b30666c81f840
Naga Durga Prasad <durgaprasadachana1108@gmail.com>
2024-06-25 15:36:25 +0530

    added responsiveness in login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Landing from "@/components/Landing";
-import Maintitle from "@/components/Maintitle";
-import Image from "next/image";
+import Landing from '@/components/Landing';
 
 export default function Home() {
-  return (
-    <div className="">
-      <Landing />
-    </div>
-  );
+    return (
+        <div className="">
+            <Landing />
+        </div>
+    );
 }
