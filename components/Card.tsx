2b92c9a9162f4a710b5948e0f5f4af402e506829
Durga Prasad <durgaprasadachana1108@gmail.com>
2024-07-30 16:02:57 +0530

    Added title to alt attribute

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,62 +1,68 @@
-import Image from "next/image";
-import { Button } from "./ui/button";
-import React from "react";
+import Image from 'next/image';
+import { Button } from './ui/button';
+import React from 'react';
 export default function Card({
-  imgUrl,
-  title,
-  btnText,
-  btnColor,
-  date,
-  handle,
-  imgBg,
-  className,
-  bottomImg,
-  bottomImgH,
-  bottomImgW,
+    imgUrl,
+    title,
+    btnText,
+    btnColor,
+    date,
+    handle,
+    imgBg,
+    className,
+    bottomImg,
+    bottomImgH,
+    bottomImgW
 }: {
-  imgUrl: string;
-  title: string;
-  btnText: string;
-  btnColor: string;
-  date: string;
-  handle: string;
-  className: string;
-  imgBg: string;
-  bottomImg?: string;
-  bottomImgW?: number;
-  bottomImgH?: number;
+    imgUrl: string;
+    title: string;
+    btnText: string;
+    btnColor: string;
+    date: string;
+    handle: string;
+    className: string;
+    imgBg: string;
+    bottomImg?: string;
+    bottomImgW?: number;
+    bottomImgH?: number;
 }) {
-  return (
-    <div
-      className={`${className} shadow-xl hover:scale-110 transition-all duration-300 hidden sm:hidden md:flex lg:flex xl:flex 2xl:flex`}
-    >
-      <div className="">
+    return (
         <div
-          className={`${imgBg} w-12 h-12 bg-white rounded-xl shadow-lg flex items-center justify-center`}
+            className={`${className} shadow-xl hover:scale-110 transition-all duration-300 hidden sm:hidden md:flex lg:flex xl:flex 2xl:flex`}
         >
-          <Image className="" src={imgUrl} alt="" width={30} height={30} />
-        </div>
-        <div className="mt-1">
-          <p className="">{title}</p>
-          <p className="text-sm font-bold">{handle}</p>
-          <p className="">{date}</p>
-        </div>
-        <Button className={`${btnColor} mt-2 h-8 rounded-full`}>
-          {btnText}
-        </Button>
-      </div>
+            <div className="">
+                <div
+                    className={`${imgBg} w-12 h-12 bg-white rounded-xl shadow-lg flex items-center justify-center`}
+                >
+                    <Image
+                        className=""
+                        src={imgUrl}
+                        alt={title}
+                        width={30}
+                        height={30}
+                    />
+                </div>
+                <div className="mt-1">
+                    <p className="">{title}</p>
+                    <p className="text-sm font-bold">{handle}</p>
+                    <p className="">{date}</p>
+                </div>
+                <Button className={`${btnColor} mt-2 h-8 rounded-full`}>
+                    {btnText}
+                </Button>
+            </div>
 
-      {bottomImg && bottomImgH && bottomImgW && (
-        <div className="mt-2 p-1">
-          <Image
-            className="rounded-lg shadow-xl border-slate-300 border"
-            src={bottomImg}
-            alt="img"
-            width={bottomImgW}
-            height={bottomImgH}
-          />
+            {bottomImg && bottomImgH && bottomImgW && (
+                <div className="mt-2 p-1">
+                    <Image
+                        className="rounded-lg shadow-xl border-slate-300 border"
+                        src={bottomImg}
+                        alt="img"
+                        width={bottomImgW}
+                        height={bottomImgH}
+                    />
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
